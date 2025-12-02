d5ac7860963adfc407b71d539af786f370e537cd
Sadaf Nazari <34862204+sadafnazari@users.noreply.github.com>
2025-12-02 22:03:06 +0200

    Updated scraper schedule

--- src/linkedin_job_search/run_scrapy.sh
+++ src/linkedin_job_search/run_scrapy.sh
@@ -7,7 +7,7 @@ source /path/to/conda/activate/script lja
 cd /path/to/Linkedin_job_analysis/src/linkedin_job_search
 
 # Run the Scrapy spider
-scrapy crawl job_scraper -a country=finland -a period=daily
+scrapy crawl job_scraper -a country=finland -a period=past_2_hours
 
 # Deactivate the conda environment
 conda deactivate
